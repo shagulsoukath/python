493608e9808277821b40bb03db8c908a7bf67ca3
shagulsoukath <51222526+shagulsoukath@users.noreply.github.com>
2019-06-06 12:21:37 +0530

    Create 3.py

---
+++ 3.py
@@ -0,0 +1,9 @@
+dk=input()
+vk=["a","e","i","o","u"]
+ck=['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z']
+if dk in vk:
+  print("Vowel")
+elif(dk in ck):
+  print("Consonant")
+else:
+  print("invalid")
