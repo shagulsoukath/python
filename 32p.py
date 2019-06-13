00d68ef4648c51fbf728ca4efa51311ed84740ba
shagulsoukath <51222526+shagulsoukath@users.noreply.github.com>
2019-06-13 10:12:14 +0530

    Create 32p.py

---
+++ 32p.py
@@ -0,0 +1,15 @@
+g=input()
+g=g.split()
+a=int(g[1])
+h=input()
+h=h.split()
+h=list(map(int,h))
+c=0
+for i in h:
+    if(i==a):
+        c=c+1
+if(c>0):
+    print("yes")
+else:
+    print("no")
+
