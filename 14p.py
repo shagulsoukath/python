1ef2232e79ef18f59993dfab786434c7389722b8
shagulsoukath <51222526+shagulsoukath@users.noreply.github.com>
2019-06-13 12:10:07 +0530

    Update 14p.py

--- 14p.py
+++ 14p.py
@@ -1,9 +1,9 @@
 d=int(input())
 f=input()
 f=list(f)
-l=[]
+o=[]
 for i in f:
     if(i!='a' and i!='e' and i!='i' and i!='0' and i!='u'):
-        l.append(i)
-h=l[::-1]
+        o.append(i)
+h=o[::-1]
 print(*h,sep='')
