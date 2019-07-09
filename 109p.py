5e37a68a21bf1af43c996caa34bde0751bc0eae3
shagulsoukath <51222526+shagulsoukath@users.noreply.github.com>
2019-07-09 18:15:03 +0530

    Create 109p.py

---
+++ 109p.py
@@ -0,0 +1,18 @@
+f=int(input())
+s=input().split()
+l=[]
+l2=[]
+c=0
+for i in s:
+    l.append(i)
+m=[]
+m.append(l[0])
+c=int(l[0])
+for j in range(0,f-1):
+    c=c+int(l[j+1])
+    m.append(c)
+d=m[::-1]
+print(*d,sep=' ')
+    
+    
+
