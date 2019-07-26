1f21abdd92a5d21a34dad063828efda727ea0d7e
shagulsoukath <51222526+shagulsoukath@users.noreply.github.com>
2019-07-26 12:00:25 +0530

    Create 4h.py

---
+++ 4h.py
@@ -0,0 +1,17 @@
+a=int(input())
+s=input().split()
+l=[]
+l2=[]
+l3=[]
+l4=[]
+for i in s:
+    l.append(i)
+for j in l:
+    if j not in l2:
+        l2.append(j)
+    else:
+        l3.append(j)
+for k in l2:
+  if k not in l3:
+      l4.append(k)
+print(*l4,sep=' ')
