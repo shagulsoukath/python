522d14915de0ad4182c3af95331a6cbf1ce59f04
shagulsoukath <51222526+shagulsoukath@users.noreply.github.com>
2019-07-26 12:12:58 +0530

    Create 7h.py

---
+++ 7h.py
@@ -0,0 +1,20 @@
+oop=int(input())
+s=input().split()
+l=[]
+l2=[]
+l3=[]
+for i in s:
+    l.append(i)
+for j in l:
+    if j not in l2:
+        l2.append(j)
+    else:
+        l3.append(j)
+        break
+d=set(l3)
+g=list(d)
+g.sort()
+if(len(g)==0):
+    print("unique")
+else:
+    print(*g,sep=' ')
